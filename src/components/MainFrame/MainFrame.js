d443fa4ba14f0ccc01d382a6015a2b8e56f76270
Oleksandr Kitsera <sashakitsera16358@gmail.com>
2019-08-04 19:34:41 +0300

    #1 Implement basic drawing.

--- src/components/MainFrame/MainFrame.js
+++ src/components/MainFrame/MainFrame.js
@@ -10,22 +10,72 @@ export default class MainFrame extends React.Component {
   constructor() {
     super();
     this.state = {
-      x: null,
-    }
+      clicking: false
+    };
   }
 
-  clickHandeler(event) {
-    const ctx = this.refs.canvas.getContext('2d')
-    rect({ ctx, x: event.clientX, y: event.clientY, width: 10, height: 10})
+  getPixelCoordinates(x, y) {
+    const rect = this.refs.canvas.getBoundingClientRect();
+    x = x - rect.x;
+    y = y - rect.y;
+    const pixelX = Math.floor(x / 32) * 32;
+    const pixelY = Math.floor(y / 32) * 32;
+    return { x: pixelX, y: pixelY };
+  }
+
+  canvas() {
+    return this.refs.canvas;
+  }
+
+  onMouseDownHandeler(event) {
+    this.setState({ clicking: true });
+    const ctx = this.canvas().getContext("2d");
+    const pixelCoordinates = this.getPixelCoordinates(
+      event.clientX,
+      event.clientY
+    );
+    rect({
+      ctx,
+      x: pixelCoordinates.x,
+      y: pixelCoordinates.y,
+      width: 32,
+      height: 32
+    });
+  }
+
+  onMouseUpHandeler(event) {
+    this.setState({ clicking: false });
+  }
+
+  onMouseMoveHandeler(event) {
+    const clicking = this.state.clicking;
+    if (clicking === false) {
+      return;
+    } else {
+      const ctx = this.canvas().getContext("2d");
+      const pixelCoordinates = this.getPixelCoordinates(
+        event.clientX,
+        event.clientY
+      );
+      rect({
+        ctx,
+        x: pixelCoordinates.x,
+        y: pixelCoordinates.y,
+        width: 32,
+        height: 32
+      });
+    }
   }
 
   render() {
     return (
       <canvas
         ref="canvas"
-        height={500}
-        width={500}
-        onMouseDown={e => this.clickHandeler(e)}
+        height={1024}
+        width={1024}
+        onMouseDown={e => this.onMouseDownHandeler(e)}
+        onMouseUp={e => this.onMouseUpHandeler(e)}
+        onMouseMove={e => this.onMouseMoveHandeler(e)}
       />
     );
   }
